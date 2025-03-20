6f876280f8157d54babd4dab3bd179a9fbbfef1d
LuisFer <lic.fernando.ortiz11@gmail.com>
2025-03-20 17:11:42 -0600

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,29 @@
+// utils.js
 function normalizarNombre(str) {
-    return str
-        .toUpperCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .replace(/\s+/g, ' ')
-        .trim();
+  return str
+    .toUpperCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s+/g, ' ')
+    .trim();
 }
 
 function getLogoUrl(empresa) {
-    switch (empresa) {
-        case "TAFER": return "./imagenes/TAFER.png";
-        case "VILLAGROUP": return "./imagenes/VILLAGROUP.png";
-        case "BALFER": return "./imagenes/BALFER.png";
-        default: return "./imagenes/default.png";
-    }
+  if (empresa === "TAFER") {
+    return "./imagenes/TAFER.png";
+  } else if (empresa === "VILLAGROUP") {
+    return "./imagenes/VILLAGROUP.png";
+  } else if (empresa === "BALFER") {
+    return "./imagenes/BALFER.png";
+  } else {
+    return "./imagenes/default.png";
+  }
 }
 
 function getSilhouetteUrl(genero) {
-    return genero === "Mujer" ? "./imagenes/siluetamujer.png" : "./imagenes/siluetahombre.png";
+  if (genero === "Mujer") {
+    return "./imagenes/siluetamujer.png";
+  } else {
+    return "./imagenes/siluetahombre.png";
+  }
 }
