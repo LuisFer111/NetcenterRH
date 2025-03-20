4842e3011a14a5d8ff51c48f44d433ab3ea4370b
LuisFer <lic.fernando.ortiz11@gmail.com>
2025-03-20 17:25:03 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // main.js
 
 document.addEventListener("DOMContentLoaded", async () => {
-  await loadEmployees(); // Carga empleados desde JSON
-  document.getElementById('employeesGrid').style.display = "none"; // Ocultamos el grid al inicio
-
-  // Una vez cargados, también mostramos ventas
-  mostrarVentasEnPagina(ventasPorEmpleado);
-});
+    await loadEmployees(); // Carga empleados desde JSON
+    document.getElementById('employeesGrid').style.display = "none"; // Ocultamos el grid al inicio
+  
+    // Una vez cargados, también mostramos ventas
+    mostrarVentasEnPagina(ventasPorEmpleado);
+  });
+  
