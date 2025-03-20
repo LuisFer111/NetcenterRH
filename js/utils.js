3cc949b56efad1fd8082227a7357128436f4911c
LuisFer <lic.fernando.ortiz11@gmail.com>
2025-03-20 17:18:09 -0600

    Add files via upload

--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,30 @@
 // utils.js
 function normalizarNombre(str) {
-  return str
-    .toUpperCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/\s+/g, ' ')
-    .trim();
-}
-
-function getLogoUrl(empresa) {
-  if (empresa === "TAFER") {
-    return "./imagenes/TAFER.png";
-  } else if (empresa === "VILLAGROUP") {
-    return "./imagenes/VILLAGROUP.png";
-  } else if (empresa === "BALFER") {
-    return "./imagenes/BALFER.png";
-  } else {
-    return "./imagenes/default.png";
+    return str
+      .toUpperCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/\s+/g, ' ')
+      .trim();
   }
-}
-
-function getSilhouetteUrl(genero) {
-  if (genero === "Mujer") {
-    return "./imagenes/siluetamujer.png";
-  } else {
-    return "./imagenes/siluetahombre.png";
+  
+  function getLogoUrl(empresa) {
+    if (empresa === "TAFER") {
+      return "./imagenes/TAFER.png";
+    } else if (empresa === "VILLAGROUP") {
+      return "./imagenes/VILLAGROUP.png";
+    } else if (empresa === "BALFER") {
+      return "./imagenes/BALFER.png";
+    } else {
+      return "./imagenes/default.png";
+    }
   }
-}
+  
+  function getSilhouetteUrl(genero) {
+    if (genero === "Mujer") {
+      return "./imagenes/siluetamujer.png";
+    } else {
+      return "./imagenes/siluetahombre.png";
+    }
+  }
+  
